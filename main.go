59ff04d9d036f1626d3b90faa2834736b38d463b
romans1212notes <romans1212notes@gmail.com>
2018-01-05 10:27:06 +0800

    Add main.go and swagger/*

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+        _ "cmdb-api/routers"
+
+        "github.com/astaxie/beego"
+        "github.com/astaxie/beego/orm"
+        _ "github.com/go-sql-driver/mysql"
+        "time"
+)
+
+func init() {
+        orm.RegisterDriver("mysql", orm.DRMySQL)
+        // set default database
+        orm.RegisterDataBase("default", "mysql", "root:<my password>@/testdb?charset=utf8", 30)
+        // Set to UTC time
+        orm.DefaultTimeLoc = time.UTC
+}
+
+func main() {
+        if beego.BConfig.RunMode == "dev" {
+                beego.BConfig.WebConfig.DirectoryIndex = true
+                beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+        }
+        beego.Run()
+}
